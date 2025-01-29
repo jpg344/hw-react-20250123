d4815be8f157d659cf74a9bc5d960db3365c62b4
jpg344 <174099082+jpg344@users.noreply.github.com>
2025-01-29 09:46:57 +0100

    added workflow for deployment on ec2

---
+++ infrastructure_ec2/main.tf
@@ -0,0 +1,64 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# VPC, Subnet, and Security Group
+resource "aws_vpc" "react_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "react_subnet" {
+  vpc_id     = aws_vpc.react_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "react_sg" {
+  name   = "ecs-security-group"
+  vpc_id = aws_vpc.react_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_internet_gateway" "react_igw" {
+  vpc_id = aws_vpc.react_vpc.id
+}
+
+resource "aws_route_table" "react_route_table" {
+  vpc_id = aws_vpc.react_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.react_igw.id
+  }
+}
+
+resource "aws_route_table_association" "react_rt_assoc" {
+  subnet_id      = aws_subnet.react_subnet.id
+  route_table_id = aws_route_table.react_route_table.id
+}
+
+
+# EC2 Instance
+resource "aws_instance" "react_ec2" {
+  ami             = "ami-0c02fb55956c7d316"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.react_subnet.id
+  security_groups = [aws_security_group.react_sg.name]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "ReactEC2"
+  }
+}
